83ed501e3f6d1f9e1a7abdcdb490a77bec0d095d
Ryan Leap <ryan.leap@gmail.com>
2020-04-13 22:11:20 -0400

    Created root module and module manifest

---
+++ BlockRunner/BlockRunner.psm1
@@ -0,0 +1,24 @@
+
+#
+# Patterned after @devblackops https://leanpub.com/building-powershell-modules
+# dot sourcing Module Layout
+#
+
+# Dot source public/private functions
+$publicFunctionsPath = Join-Path -Path $PSScriptRoot -ChildPath 'Public/*.ps1'
+$privateFunctionsPath = Join-Path -Path $PSScriptRoot -ChildPath 'Private/*.ps1'
+$public = @(Get-ChildItem -Path $publicFunctionsPath -Recurse -ErrorAction Stop)
+$private = @(Get-ChildItem -Path $privateFunctionsPath -Recurse -ErrorAction Stop)
+foreach ($file in @($public + $private)) {
+    try {
+        . $file.FullName
+    }
+    catch {
+        throw "Unable to dot source [$($file.FullName)]"
+    }
+}
+
+# Define aliases
+$aliases = @()
+$aliases += New-Alias -Name nbr -Value New-BlockRunner -PassThru
+Export-ModuleMember -Function $public.BaseName -Alias $aliases.Name
